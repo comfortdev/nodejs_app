c7d6177180b27afbae704e6814d9e7870116f9eb
comfortdev <comfortdev1@gmail.com>
2022-06-26 21:56:25 +0300

    Added unit tests.

---
+++ __tests__/app/helpers/math.test.js
@@ -0,0 +1,21 @@
+const mathHelper = require('../../../app/helpers/math');
+
+describe('Math helper tests', () => {
+
+    test('addInteger returns expected result', () => {
+        const expectedResult = 8;
+        const int1 = 6;
+        const int2 = 2;
+
+        expect(mathHelper.addInteger(int1, int2)).toEqual(expectedResult);
+    });
+
+    test('addInteger throw error for non numbers arguments', () => {
+        const int1 = 6;
+        const int2 = '2';
+
+        expect(() => {
+            mathHelper.addInteger(int1, int2);
+        }).toThrow(Error('Invalid type of arguments, only number is allowed here.'));
+    })
+});
