0a48f06502c26d0d4c94752f37e802a063af4e99
comfortdev <comfortdev1@gmail.com>
2022-06-26 21:55:43 +0300

    Added math helper.

---
+++ app/helpers/math.js
@@ -0,0 +1,10 @@
+function addInteger(int1, int2) {
+    if (typeof int1 !== 'number' || typeof int2 !== 'number') {
+        throw new Error('Invalid type of arguments, only number is allowed here.')
+    }
+    return int1 + int2;
+}
+
+module.exports = {
+    addInteger
+}
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('my_nodejs_app')
+const mathHelper = require('./app/helpers/math');
+
+const result = mathHelper.addInteger(3, 4);
+console.log(result);
