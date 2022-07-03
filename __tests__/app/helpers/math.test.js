d72bacc9fae1494e04fbd2950d2e29711e2dfcd8
comfortdev <comfortdev1@gmail.com>
2022-07-03 21:51:17 +0300

    Fixed lint

--- __tests__/app/helpers/math.test.js
+++ __tests__/app/helpers/math.test.js
@@ -1,21 +1,20 @@
-const mathHelper = require('../../../app/helpers/math');
+const mathHelper = require('../../../app/helpers/math')
 
 describe('Math helper tests', () => {
+  test('addInteger returns expected result', () => {
+    const expectedResult = 8
+    const int1 = 6
+    const int2 = 2
 
-    test('addInteger returns expected result', () => {
-        const expectedResult = 8;
-        const int1 = 6;
-        const int2 = 2;
+    expect(mathHelper.addInteger(int1, int2)).toEqual(expectedResult)
+  })
 
-        expect(mathHelper.addInteger(int1, int2)).toEqual(expectedResult);
-    });
+  test('addInteger throw error for non numbers arguments', () => {
+    const int1 = 6
+    const int2 = '2'
 
-    test('addInteger throw error for non numbers arguments', () => {
-        const int1 = 6;
-        const int2 = '2';
-
-        expect(() => {
-            mathHelper.addInteger(int1, int2);
-        }).toThrow(Error('Invalid type of arguments, only number is allowed here.'));
-    })
-});
+    expect(() => {
+      mathHelper.addInteger(int1, int2)
+    }).toThrow(Error('Invalid type of arguments, only number is allowed here.'))
+  })
+})
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mathHelper = require('./app/helpers/math');
+const mathHelper = require('./app/helpers/math')
 
-const result = mathHelper.addInteger(3, 4);
-console.log(result);
+const result = mathHelper.addInteger(3, 4)
+console.log(result)
